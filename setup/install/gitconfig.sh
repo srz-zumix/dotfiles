cdd46ab919ac7b04c382585316ba9d3f2a412203
srz_zumix <zumix.cpp@gmail.com>
2019-01-30 10:27:16 +0900

    add chmod alias

--- setup/install/gitconfig.sh
+++ setup/install/gitconfig.sh
@@ -9,7 +9,8 @@ for f in `find $ROOT_DIR/git/config -type f -name '*.config'`; do
             tag=`echo ${line} | cut -d"[" -f2 | cut -d"]" -f1`
         else
             command=`echo ${line} | cut -d"=" -f1`
-            raw__=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f1`
+            raw__=`echo ${line} | cut -d"=" -f2-`
+            # raw__=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f1`
             raw_=${raw__## }
             raw=${raw_% }
             value_=${raw#\"}
