8ff98d1973c2f97ce114da8cab0091e773557ce3
srz_zumix <zumix.cpp@gmail.com>
2019-01-31 09:53:33 +0900

    update git config

--- setup/install/gitconfig.sh
+++ setup/install/gitconfig.sh
@@ -2,22 +2,35 @@
 
 backup .gitconfig
 
-for f in `find $ROOT_DIR/git/config -type f -name '*.config'`; do
-    echo $f
-    while read line; do
-        if [[ $line =~ ^\[.*\] ]]; then
-            tag=`echo ${line} | cut -d"[" -f2 | cut -d"]" -f1`
-        else
-            command=`echo ${line} | cut -d"=" -f1`
-            raw__=`echo ${line} | cut -d"=" -f2-`
-            # raw__=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f1`
-            raw_=${raw__## }
-            raw=${raw_% }
-            value_=${raw#\"}
-            value=${value_%\"}
-            # comment=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f2-`
-            echo $tag.$command $value $comment
-            git config --global --replace-all $tag.$command "${value}"
-        fi
-    done < $f
-done
+function git_config_update() {
+  for f in `find $ROOT_DIR/$1 -type f -name '*.config'`; do
+      echo $f
+      while read line; do
+          if [[ $line =~ ^\[.*\] ]]; then
+              tag=`echo ${line} | cut -d"[" -f2 | cut -d"]" -f1`
+          else
+              if [[ $line =~ "^#" ]]; then
+                # comment=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f2-`
+                echo $line
+              else
+                command=`echo ${line} | cut -d"=" -f1`
+                raw__=`echo ${line} | cut -d"=" -f2-`
+                # raw__=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f1`
+                raw_=${raw__## }
+                raw=${raw_% }
+                value_=${raw#\"}
+                value=${value_%\"}
+                # comment=`echo ${line} | cut -d"=" -f2- | cut -d"#" -f2-`
+                echo $tag.$command $value $comment
+                git config --global --replace-all $tag.$command "${value}"
+              fi
+          fi
+      done < $f
+  done
+}
+
+git_config_update git/config
+
+# if [ is_windows ]; then
+#   git_config_update git/windows/config
+# fi
