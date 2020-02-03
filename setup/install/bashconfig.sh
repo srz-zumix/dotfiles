8c5b8bff68c57e6e1ac1be99adbfad25ca51eb55
srz_zumix <zumix.cpp@gmail.com>
2020-02-03 16:59:51 +0900

    Update bashconfig.sh

--- setup/install/bashconfig.sh
+++ setup/install/bashconfig.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+# for bash
+
 backup .bashrc
 backup .bash_profile
 
 make_link .bashrc
 make_link .bash_profile
 
+# for zsh
+function zshconfig() {
+    backup .zprofile
+    backup .zshrc
 
+    make_link .zprofile
+    make_link .zshrc
+}
+if !(type "zsh" > /dev/null 2>&1); then
+    zshconfig
+fi
